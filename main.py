8b439e8d31e0fb0fa3f368da3c36338dfef8cdee
damianmolina <damian.a.molina@gmail.com>
2023-04-10 19:57:31 -0400

    First level of mario (scaled)

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from cmu_graphics import *
 from PIL import Image
 
 def onAppStart(app):
-    app.image = Image.open('Mario 1-1.png')
+    app.width = 896
+    app.height = 448
+    app.image = Image.open('Images/Mario 1-1.png')
+    app.image = app.image.resize((app.image.width * 2, app.image.height * 2))
     app.image = CMUImage(app.image)
 
 
 def redrawAll(app):
     drawImage(app.image, 0, 0)
 
-runApp(width = 448, height = 224)
+runApp(app.width, app.height)
