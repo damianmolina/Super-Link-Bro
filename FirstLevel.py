22ec25cacb090588b32f29c9b2f26e8dc4ee12fb
damianmolina <damian.a.molina@gmail.com>
2023-04-11 12:15:27 -0400

    Floor drawn

--- FirstLevel.py
+++ FirstLevel.py
@@ -3,4 +3,25 @@ from PIL import Image
 from Characters import *
 
 def drawFirstLevel(app):
+    # First draw the background
     drawImage(CMUImage(app.firstLevel), 0, 0)
+    for col in range(app.cols):
+        drawCell(app, app.rows, col)
+
+def drawCell(app, row, col):
+    cellLeft, cellTop = getCellLeftTop(app, row, col)
+    cellWidth, cellHeight = getCellSize(app)
+    drawRect(cellLeft, cellTop, cellWidth, cellHeight, fill = 'black',
+             border='black', borderWidth = app.cellBorderWidth)
+
+def getCellLeftTop(app, row, col):
+    cellWidth, cellHeight = getCellSize(app)
+    cellLeft = app.levelLeft + col * cellWidth
+    cellTop = app.levelTop + row * cellHeight
+    return (cellLeft, cellTop)
+
+def getCellSize(app):
+    cellWidth = app.levelWidth / app.cols
+    cellHeight = app.levelHeight / app.rows
+    return (cellWidth, cellHeight)
+
--- main.py
+++ main.py
@@ -4,21 +4,41 @@ from Characters import *
 from FirstLevel import *
 
 def onAppStart(app):
+    # Screen width and height
     app.width = 896
     app.height = 448
+
+    # Get the first level image
     app.firstLevel = Image.open('Images/Mario 1-1.png')
+    print(app.firstLevel.size)
     app.firstLevel = app.firstLevel.resize((app.firstLevel.width * 2, app.firstLevel.height * 2))
+    print(app.firstLevel.size)
+    
+    # Create Link object
     app.link = Link()
+
+    # Attributes to track where the mouse is
     app.labelX = 0
     app.labelY = 0
+
+    # Tells whether Link is moving right or left
     app.moveRight = False
     app.moveLeft = False
+
+    # Attributes for grid in the background
+    app.rows = 13
+    app.cols = 212
+    app.levelLeft = 0
+    app.levelTop = 0
+    app.levelWidth = 6768
+    app.levelHeight = 400
+    app.cellBorderWidth = 1
 
 
 def redrawAll(app):
     drawFirstLevel(app)
     drawImage(CMUImage(app.link.image), app.link.leftX, app.link.topY)
-    drawLabel(f'({app.labelX}, {app.labelY})', app.labelX, app.labelY)
+    drawLabel(f'({app.labelX}, {app.labelY})', app.labelX, app.labelY - 10)
 
 
 def onMouseMove(app, mouseX, mouseY):
